ff576e8ded674eacacc5dd816f35f0c645c8a1a5
hivislav <hivislav@gmail.com>
2022-03-30 19:10:32 +0500

    Домашняя работа №11.1
    //реализовано контекстное меню на выделенной заметке

--- app/src/main/java/ru/geekbrains/noteapphomework/NotesListFragment.java
+++ app/src/main/java/ru/geekbrains/noteapphomework/NotesListFragment.java
@@ -3,11 +3,14 @@ package ru.geekbrains.noteapphomework;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,6 +30,7 @@ public class NotesListFragment extends Fragment implements NotesAdapter.OnNoteCl
     private RecyclerView list;
     private Repo repo = InMemoryRepoImp.getInstance();
     private NotesAdapter adapter;
+    private Note note;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,7 +41,7 @@ public class NotesListFragment extends Fragment implements NotesAdapter.OnNoteCl
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         list = view.findViewById(R.id.list);
-        adapter = new NotesAdapter();
+        adapter = new NotesAdapter(this);
 
         //функция для записи щелчков
         adapter.setOnNoteClickListener(this);
@@ -57,9 +61,47 @@ public class NotesListFragment extends Fragment implements NotesAdapter.OnNoteCl
             ((Controller) requireActivity()).openEditNoteFragment(note);
         }
 
+    @Override
+    public void onNoteLongClick(Note note) {
+        this.note = note;
+    }
+
     public void refresh() {
         if(adapter != null)
             adapter.notifyDataSetChanged();
     }
 
+
+
+    //реализация контекстного меню на фрагменте
+    @Override
+    public void onCreateContextMenu(@NonNull ContextMenu menu, @NonNull View v, @Nullable ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = requireActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.note_context_menu, menu);
+    }
+
+    //TODO 30.03
+    // 1. реализовать шеринг
+    // 2. реализовать диалоговое окно с подтверждением удаления заметки
+    // 3. реализовать анимацию при удалении (т.к. мы знаем конкретный id удаляемой заметки)
+    // 4. реализовать закрытие окна с редактированием заметки при ее удалении в альбомной ориентации
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.context_menu_change_note:
+                ((Controller) requireActivity()).openEditNoteFragment(note);
+                return true;
+            case R.id.context_menu_delete_note:
+                repo.delete(note.getId());
+                refresh();
+                return true;
+            case R.id.context_menu_share_note:
+                Toast.makeText(requireActivity(),"Здесь будет возможность поделиться", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 }
--- app/src/main/java/ru/geekbrains/noteapphomework/recycler/NoteHolder.java
+++ app/src/main/java/ru/geekbrains/noteapphomework/recycler/NoteHolder.java
@@ -4,6 +4,8 @@ import android.view.View;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import ru.geekbrains.noteapphomework.NotesListFragment;
 import ru.geekbrains.noteapphomework.R;
 import ru.geekbrains.noteapphomework.data.Note;
 
@@ -12,18 +14,31 @@ public class NoteHolder extends RecyclerView.ViewHolder {
     private TextView title;
     private TextView description;
     private Note note;
+    private NotesListFragment notesListFragment;
+    public int noteHolderPosition;
 
 
-    public NoteHolder(@NonNull View itemView, NotesAdapter.OnNoteClickListener listener) {
+
+    public NoteHolder(@NonNull View itemView, NotesAdapter.OnNoteClickListener listener, NotesListFragment notesListFragment) {
         super(itemView);
+        this.notesListFragment = notesListFragment;
         title = itemView.findViewById(R.id.note_title);
         description = itemView.findViewById(R.id.note_descripton);
+        registerContextMenu(itemView);
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 listener.onNoteClick(note);
             }
         });
+        itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                listener.onNoteLongClick(note);
+                return false;
+            }
+        });
+
     }
 
     void bind(Note note)
@@ -31,5 +46,12 @@ public class NoteHolder extends RecyclerView.ViewHolder {
         this.note = note;
         title.setText(note.getTitle());
         description.setText(note.getDescription());
+    }
+
+
+//Метод для регистрации контекстного меню на конкретном холдере
+    private void registerContextMenu(@NonNull View itemView) {
+        if (notesListFragment != null)
+            notesListFragment.registerForContextMenu(itemView);
     }
 }
